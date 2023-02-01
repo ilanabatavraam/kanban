c0c6b416a2132c3dad022b453bb19474d654951b
Ilana Sandler <9151o66926@gmail.com>
2023-02-01 21:23:36 +0200

    one line done

--- src/components/TasksLine.tsx
+++ src/components/TasksLine.tsx
@@ -5,13 +5,15 @@ export function TasksLine() {
         setValue((event.target as HTMLInputElement).value)
     }
 
+    const dataStr: string = 'Open'
+
     return (
         <div data-name="open" className="flex flex-col gap-2 w-1/4">
-            <h3 className="bg-gray-200 uppercase w-fit p-1 pl-4 pr-3 rounded">Open</h3>
+            <h3 className="bg-gray-200 uppercase w-fit p-1 pl-4 pr-3 rounded">{dataStr}</h3>
             <ul className="flex flex-col gap-2">
                 <li className="rounded p-3 border cursor-pointer border-l-4 border-l-gray-200">
                     <input
-                        value="Task name 1"
+                        value={value ? value : dataStr}
                         className="border-2 outline-0 border-transparent hover:border-2 hover:border-gray-400 hover:border-dashed"
                         onInput={changeHandler}
                     />
